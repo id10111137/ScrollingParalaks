068456d2cb7aea1bdf64f0f112a52c347fc9cc94
Tatang R Ganda Wijaya <id.10111137@gmail.com>
2019-06-24 10:29:27 +0700

    Initial commit

---
+++ app/src/main/java/com/tatang/pcit1/scrollingparalak/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tatang.pcit1.scrollingparalak;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
